48411a9022567e1e97c76d94c2458736f629baa4
valyan <vally-vlabo@yandex.ru>
2024-09-30 21:26:28 +0300

    Добавьте файлы проекта.

---
+++ DZSemi4Patterns/ChatServer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZSemi4Patterns
+{
+    public class ChatServer
+    {
+        private  Queue<ICommand> _commandQueue = new Queue<ICommand>();
+
+        public void AddCommand(ICommand command)
+        {
+            _commandQueue.Enqueue(command);
+        }
+
+        public void ProcessCommands()
+        {
+            while (_commandQueue.Count > 0)
+            {
+                var command = _commandQueue.Dequeue();
+                command.Execute();
+            }
+        }
+    }
+}
---
+++ DZSemi4Patterns/PrototypePattern.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZSemi4Patterns
+{
+    internal class PrototypePattern : ICloneable 
+        // Prototype
+    {
+        public string ConstantText = "With best regards";
+        public Message Message { get; set; }
+
+        public PrototypePattern( Message message)
+        {
+           
+            Message = message;
+        }
+        
+        public object Clone()
+        {
+            Message.Text += ConstantText;
+            return Message as object;
+           
+        }
+    }
+}
